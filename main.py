b468031012b657aa5820f9c8e02ae33b29e37a55
Hytalo Bassi <hytalo-bassi@protonmail.com>
2024-08-15 22:46:49 -0400

    feat: adds more operators and better parenthesis support

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import inspect
-import re
 from enum import Enum
 from rich.console import Console
 from rich.table import Table
@@ -8,9 +6,8 @@ from rich.table import Table
 class Operations(Enum):
     AND = 0
     OR = 1
-
-
-Operations = Enum('Operations', ['AND', 'OR'])
+    IMPLIES = 2
+    DUAL_IMPLICATION = 3
 
 
 def get_operation(char: str):
@@ -112,8 +109,16 @@ class Node(Logic):
     def get(self):
         if self.op == Operations.AND:
             return self.left.get() and self.right.get()
-        else:
+        elif self.op == Operations.OR:
             return self.left.get() or self.right.get()
+        elif self.op == Operations.IMPLIES:
+            if self.left.get() and not self.right.get():
+                return False
+            return True
+        elif self.op == Operations.DUAL_IMPLICATION:
+            if self.left.get() is not self.right.get():
+                return False
+            return True
 
 
     def to_expr(self):
@@ -124,8 +129,16 @@ class Node(Logic):
             left_expr = f"({left_expr})"
         if isinstance(self.right, Node):
             right_expr = f"({right_expr})"
-
-        middle = " ∨ " if self.op == Operations.OR else " ∧ "
+        
+        middle = ""
+        if self.op == Operations.OR:
+            middle = " ∨ "
+        elif self.op == Operations.AND:
+            middle = " ∧ "
+        elif self.op == Operations.IMPLIES:
+            middle = " → "
+        elif self.op == Operations.DUAL_IMPLICATION:
+            middle = " ≡ "
 
         return left_expr + middle + right_expr
 
@@ -135,6 +148,36 @@ def get_truth_row(varlist):
     for var in varlist:
         row.append(str(var.get())[0])
     return row
+
+
+def tokenize(s: str):
+    # ensures that the parser will be able to understand the correct order.
+    open_parens = 0
+    tokens = []
+    size = len(s)
+    for i in range(0, size):
+        char = s[i]
+        if char == '(':
+            open_parens += 1
+        elif char == ')':
+            open_parens -= 1
+        else:
+            if open_parens == 0:
+                if char == '+':
+                    tokens.append(token(i, Operations.OR))
+                elif char == '*':
+                    tokens.append(token(i, Operations.AND))
+                elif char == '-' and i < size - 1 and s[i + 1] == '>':
+                    tokens.append(token(i, Operations.IMPLIES, end = i + 1))
+
+    return tokens
+
+
+def token(start: int, score: int, end: int = None):
+    if end is None:
+        end = start
+
+    return { 'start': start, 'end': end, 'score': score }
 
 
 def get_tokens(s: str):
@@ -149,20 +192,20 @@ def get_tokens(s: str):
     return tokens
 
 
-def left_and_right(s: str, i: int):
-    return [s[:i], s[i + 1:]]
+def left_and_right(s: str, token):
+    return s[:token['start']], s[token['end'] + 1:]
 
 
-def least_precedence(tokens: dict):
-    if len(tokens) == 0: return -1
+def least_precedence(tokens):
+    if len(tokens) == 0:
+        return -1
 
-    M = tokens[0]['score']
+    M = tokens[0]['score'].value
     i_max = 0
     i = 0
 
     while (i < len(tokens)):
-        token = tokens[i]
-        token_scr = token['score']
+        token_scr = tokens[i]['score'].value
         
         if token_scr > M:
             i_max = i
@@ -172,11 +215,31 @@ def least_precedence(tokens: dict):
     return i_max
 
 
-def _parser(expr: str, symboldict: dict, bits):
-    tokens = get_tokens(expr)
-    current_token_i = least_precedence(tokens)
+def rm_outer_parenthesis(s: str):
+    open_parens = 0
+    # tests if s is a expr like (...) or a composition of expressions
+    # by an operator like (...) + (...) * (...).
+    for char in s:
+        if char == '(':
+            open_parens += 1
+        elif char == ')':
+            open_parens -= 1
+        else:
+            if open_parens == 0:
+                return s
     
-    if current_token_i < 0:
+    # if it's just a big (...) then it's safe to remove the parenthesis.
+    if s[0] == '(' and s[len(s) - 1] == ')':
+        return s[1:len(s) - 1]
+    return s
+
+
+def _parser(expr: str, symboldict: dict, bits):
+    expr = rm_outer_parenthesis(expr)
+    tokens = tokenize(expr)
+    last_token = least_precedence(tokens)
+    
+    if last_token < 0:
         # if variable is already known
         if expr in symboldict:
             return symboldict[expr]
@@ -188,15 +251,15 @@ def _parser(expr: str, symboldict: dict, bits):
         
         return v
 
-    op_i = tokens[current_token_i]['i']
+    left_expr, right_expr = left_and_right(expr, tokens[last_token])
     
-    op = expr[op_i]
-    left_expr, right_expr = left_and_right(expr, op_i)
-    
+    left_expr = rm_outer_parenthesis(left_expr)
+    right_expr = rm_outer_parenthesis(right_expr)
+
     left_node = _parser(left_expr, symboldict, bits)
     right_node = _parser(right_expr, symboldict, bits)
     
-    n = Node(left = left_node, right = right_node, op = get_operation(op))
+    n = Node(left = left_node, right = right_node, op = tokens[last_token]['score'])
     return n
 
 
