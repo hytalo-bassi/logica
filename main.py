e5bb392be482cc2771fc7747fb7bdf53fbdcee3a
Hytalo Bassi <hytalo-bassi@protonmail.com>
2024-08-18 19:14:08 -0400

    feat: adds Truth and Falsity

--- main.py
+++ main.py
@@ -12,25 +12,24 @@ class Operations(Enum):
 
 
 def get_operation(char: str):
-    if char == '+':
+    if char == "+":
         return Operations.OR
 
     return Operations.AND
 
-class Logic:
 
+class Logic:
 
     def value(self):
         return
-
 
     def to_expr(self):
         return
 
 
 class Bits:
-    bits = ''
-    
+    bits = ""
+
     @property
     def size(self):
         print(self.bits)
@@ -38,31 +37,26 @@ class Bits:
 
     @staticmethod
     def get_bits(n: int = 1):
-        return '0' * n
-
+        return "0" * n
 
     def __init__(self, n: int = 1):
         self.bits = Bits.get_bits(n)
-    
 
     def at(self, i: int = 0):
         if i < 0 or i > len(self.bits):
             i = 0
-        return self.bits[i] == '1'
+        return self.bits[i] == "1"
 
-    
     def set(self, n: int):
         size = len(self.bits)
         self.bits = bin(n)[2:].zfill(size)
 
-
     def __add__(self, n: int):
-        v = int(self.bits, base = 2) + n
-        if v > 2**len(self.bits):
+        v = int(self.bits, base=2) + n
+        if v > 2 ** len(self.bits):
             v = 0
-        
-        self.set(v)
 
+        self.set(v)
 
     def realloc(self, x: int = 1):
         size = len(self.bits)
@@ -72,26 +66,34 @@ class Bits:
 
 class Variable(Logic):
     i = None
-    symbol = 'None'
+    symbol = "None"
     bits = None
+    fixed_value = None
 
-
-    def __init__(self, bits: Bits, i = 0, symbol = 'None'):
+    def __init__(self, bits: Bits = None, i=0, symbol="None", fixed_value: bool = None):
+        if fixed_value is not None:
+            self.fixed_value = fixed_value
+            self.symbol = "T" if fixed_value else "F"
+            return
         self.bits = bits
         self.i = i
         self.symbol = symbol
 
-
     def get(self):
+        if self.fixed_value is not None:
+            return self.fixed_value
         return self.bits.at(self.i)
-
 
     def value(self):
         return self.get()
 
-
     def to_expr(self):
         return self.symbol
+
+
+# Useful when you dont want to have multiple possible values
+Truth = Variable(fixed_value=True)
+Falsity = Variable(fixed_value=False)
 
 
 class Node(Logic):
@@ -99,13 +101,11 @@ class Node(Logic):
     right = None
     op = None
     var = None
-    
 
-    def __init__(self, left = None, right = None, op: int = None):
+    def __init__(self, left=None, right=None, op: int = None):
         self.left = left
         self.right = right
         self.op = op
-    
 
     def get(self):
         if self.op == Operations.AND:
@@ -123,7 +123,6 @@ class Node(Logic):
         elif self.op == Operations.NOT:
             return not self.right.get()
 
-
     def to_expr(self):
         left_expr = self.left.to_expr() if self.left is not None else None
         right_expr = self.right.to_expr()
@@ -132,7 +131,7 @@ class Node(Logic):
             left_expr = f"({left_expr})"
         if isinstance(self.right, Node) and self.right.op != Operations.NOT:
             right_expr = f"({right_expr})"
-        
+
         middle = ""
         if self.op == Operations.OR:
             middle = " ∨ "
@@ -167,22 +166,22 @@ def tokenize(s: str):
 
     while i < size:
         char = s[i]
-        if char == '(':
+        if char == "(":
             open_parens += 1
-        elif char == ')':
+        elif char == ")":
             open_parens -= 1
         else:
             if open_parens == 0:
-                if char == '+':
+                if char == "+":
                     tokens.append(token(i, Operations.OR))
-                elif char == '*':
+                elif char == "*":
                     tokens.append(token(i, Operations.AND))
-                elif char == '-' and i < size - 1 and s[i + 1] == '>':
-                    tokens.append(token(i, Operations.IMPLIES, end = i + 1))
-                elif char == '<' and i < size - 2 and s[i:i + 3] == '<->':
-                    tokens.append(token(i, Operations.DUAL_IMPLICATION, end = i + 2))
+                elif char == "-" and i < size - 1 and s[i + 1] == ">":
+                    tokens.append(token(i, Operations.IMPLIES, end=i + 1))
+                elif char == "<" and i < size - 2 and s[i : i + 3] == "<->":
+                    tokens.append(token(i, Operations.DUAL_IMPLICATION, end=i + 2))
                     i += 1
-                elif char == '~':
+                elif char == "~":
                     tokens.append(token(i, Operations.NOT))
         i += 1
 
@@ -193,15 +192,16 @@ def token(start: int, score: int, end: int = None):
     if end is None:
         end = start
 
-    return { 'start': start, 'end': end, 'score': score }
+    return {"start": start, "end": end, "score": score}
 
 
 def left_and_right(s: str, token):
-    return s[:token['start']], right_side(s, token)
+    return s[: token["start"]], right_side(s, token)
 
 
 def right_side(s: str, token):
-    return s[token['end'] + 1:]
+    return s[token["end"] + 1 :]
+
 
 def least_precedence(tokens):
     if len(tokens) == 0:
@@ -210,15 +210,15 @@ def least_precedence(tokens):
     i_max = 0
     i = 0
 
-    while (i < len(tokens)):
-        M = tokens[i_max]['score'].value
-        token_scr = tokens[i]['score'].value
-        
+    while i < len(tokens):
+        M = tokens[i_max]["score"].value
+        token_scr = tokens[i]["score"].value
+
         if token_scr > M:
             i_max = i
-        
+
         i += 1
-    
+
     return i_max
 
 
@@ -227,17 +227,17 @@ def rm_outer_parenthesis(s: str):
     # tests if s is a expr like (...) or a composition of expressions
     # by an operator like (...) + (...) * (...).
     for char in s:
-        if char == '(':
+        if char == "(":
             open_parens += 1
-        elif char == ')':
+        elif char == ")":
             open_parens -= 1
         else:
             if open_parens == 0:
                 return s
-    
+
     # if it's just a big (...) then it's safe to remove the parenthesis.
-    if s[0] == '(' and s[len(s) - 1] == ')':
-        return s[1:len(s) - 1]
+    if s[0] == "(" and s[len(s) - 1] == ")":
+        return s[1 : len(s) - 1]
     return s
 
 
@@ -245,24 +245,26 @@ def _parser(expr: str, symboldict: dict, bits):
     expr = rm_outer_parenthesis(expr)
     tokens = tokenize(expr)
     last_token = least_precedence(tokens)
-    
+
     if last_token < 0:
+        if expr in ["T", "F"]:
+            return Truth if expr == "T" else Falsity
         # if variable is already known
         if expr in symboldict:
             return symboldict[expr]
 
         i = bits.realloc()
 
-        v = Variable(bits, symbol = expr, i = i)
+        v = Variable(bits, symbol=expr, i=i)
         symboldict[expr] = v
-        
+
         return v
 
     left_node = None
     right_node = None
-    if tokens[last_token]['score'] != Operations.NOT:
+    if tokens[last_token]["score"] != Operations.NOT:
         left_expr, right_expr = left_and_right(expr, tokens[last_token])
-        
+
         left_expr = rm_outer_parenthesis(left_expr)
         right_expr = rm_outer_parenthesis(right_expr)
 
@@ -273,7 +275,7 @@ def _parser(expr: str, symboldict: dict, bits):
         right_expr = rm_outer_parenthesis(right_expr)
 
         right_node = _parser(right_expr, symboldict, bits)
-    n = Node(left = left_node, right = right_node, op = tokens[last_token]['score'])
+    n = Node(left=left_node, right=right_node, op=tokens[last_token]["score"])
     return n
 
 
@@ -291,11 +293,11 @@ def main():
     i = 0
     table = []
     for var in s:
-        tab_rich.add_column(var, justify = "center")
+        tab_rich.add_column(var, justify="center")
         table.append(s[var])
-    
-    tab_rich.add_column(n.to_expr(), justify = "center")
-    for i in range(0, 2**len(s)):
+
+    tab_rich.add_column(n.to_expr(), justify="center")
+    for i in range(0, 2 ** len(s)):
         tab_rich.add_row(*get_truth_row(table), str(n.get())[0])
         bits + 1
 
@@ -304,4 +306,3 @@ def main():
 
 
 main()
-
